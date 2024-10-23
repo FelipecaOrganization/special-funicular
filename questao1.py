318843d92a18370a7312408b3fdaa8bc82c5b2a3
Felipe Cassano <138160648+FelipeCassanoA@users.noreply.github.com>
2024-10-23 19:24:21 -0300

    Update questao1.py

--- questao1.py
+++ questao1.py
@@ -4,3 +4,27 @@ def questao1 (b,c):
 def divisao(a,b):
   return a/b
   
+def questao1 (b,c):
+  return b-c
+
+def divisao(a,b):
+  return a/b
+
+def questao1 (b,c):
+  return b-c
+
+def divisao(a,b):
+  return a/b
+
+def questao1 (b,c):
+  return b-c
+
+def divisao(a,b):
+  return a/b
+
+def questao1 (b,c):
+  return b-c
+
+def divisao(a,b):
+  return a/b
+  
